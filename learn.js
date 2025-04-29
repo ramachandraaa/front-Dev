d6294ba53da00ff734a1f4e9851dc055fe59bec4
ramachandraaa <159449668+ramachandraaa@users.noreply.github.com>
2025-04-29 15:56:51 +0530

    djfhd

---
+++ learn.js
@@ -0,0 +1,15 @@
+let a=10
+
+console.log("Ramachandra"+a)
+
+if(a<=10)
+{
+    console.log("hey the number is lesss than 10")
+    
+}
+
+function greet(name)
+{
+    return "Hello "+name;
+}
+console.log(greet("Ramachandra"))
